919ede21163effb85d25af0fc72564727a6c6f5a
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-09 22:55:06 +0200

    do not generate excel

--- Track_and_trace.py
+++ Track_and_trace.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
-name = 'Tarka'
-batch = '2018/12/09'
-expire = '2020/12'
+def product_code_lines(name, batch, expire):
+    
+    read_code=pd.read_csv('codes.csv')
+    code_from_file = str(read_code.iloc[:1]).strip().split()[2]
+    batch_and_expire = str(batch+'/'+expire)
+    product_code = batch_and_expire+'/'+code_from_file
+    product_code_frame=pd.DataFrame({
+                                'Code': [code_from_file],
+                                'Batch and expire': [batch_and_expire],
+                                'Product Code': [product_code],
+                                'Product name': [name],
+                                'Box': [0],
+                                'Pallet': [0],
+                                'Batch': [batch],
+                                'Expire date': [expire]})
+    return product_code_frame
+    # print(product_code_frame)
+    product_code_frame.to_excel('Output.xlsx', index=False)
+    
 
-read_code=pd.read_csv('codes.csv')
-code_from_file = str(read_code.iloc[:1]).strip().split()[2]
-batch_and_expire = str(batch+'/'+expire)
-product_code = batch_and_expire+'/'+code_from_file
-product_code_frame=pd.DataFrame({
-                            'Code': [code_from_file],
-                            'Batch and expire': [batch_and_expire],
-                            'Product Code': [product_code],
-                            'Product name': [name],
-                            'Box': [0],
-                            'Pallet': [0],
-                            'Batch': [batch],
-                            'Expire date': [expire]})
-
-
-print(product_code_frame)
-
-product_code_frame.to_excel('Output.xlsx', index=False)
+product_code_lines('Tarka','2018/12/09','2020/12')
+    
