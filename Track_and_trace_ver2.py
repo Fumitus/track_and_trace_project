3ddb8c5b5d6edffdb86d9da97f095d87295ff843
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-11 22:43:48 +0200

    code added

--- Track_and_trace_ver2.py
+++ Track_and_trace_ver2.py
@@ -9,6 +9,7 @@ name = 'Tarka'
 batch = '2018/12/09'
 expire = '2020/12'
 
+
 # def read_code_from_file():
 #     code_from_file = pd.read_csv('codes.csv')
 #     return code_from_file
@@ -43,53 +44,83 @@ expire = '2020/12'
 
 
 
-# def join_pallet(box_code):
-#     pallet = []
-#     while pallet <= 10:
-#         pallet.append(box_code)
-#     else:
-#         pallet_code = batch_and_expire +'/'+ code_from_file
-#     return pallet_code
+def join_pallet(box_code):
+    pallet = []
+    while pallet <= 10:
+        pallet.append(box_code)
+    else:
+        pallet_code = batch_and_expire +'/'+ code_from_file
+    return pallet_code
+
+product_code = ['0000000SARIDONAAAAA20202020', 
+                '0000001SARIDONAAAAA20202020', 
+                '0000002SARIDONAAAAA20202020', 
+                '0000003SARIDONAAAAA20202020', 
+                '0000004SARIDONAAAAA20202020', 
+                '0000005SARIDONAAAAA20202020',
+                '0000006SARIDONAAAAA20202020']
+box_code = ['0000007SARIDONAAAAA20202020', '0000008SARIDONAAAAA20202020']
+pallet_code =['0000009SARIDONAAAAA20202020']
+
+
+
+product_code_frame = {'Code': [], 'Product name': [], 'Batch': [], 'Exp date': [], 'Product Code': [], 'Box_code': [], 'Pallet_code': []}
+product_code_frame['Code'].append(product_code)
+product_code_frame['Product name'].append(name)
+product_code_frame['Batch'].append(batch)
+product_code_frame['Exp date'].append(expire)
+product_code_frame['Product Code'].append(product_code)
+product_code_frame['Box_code'].append(box_code)
+product_code_frame['Pallet_code'].append(pallet_code)
+
+print(product_code_frame)
+print(product_code_frame['Pallet_code'])
+
+
+
+
+
 
 # header_keys = {'Code': [None], 'Product name': [name], 'Batch': [batch], 'Expire date': [expire],'Product Code': [None], 'Box': [None], 'Pallet': [None]}
 
-product_code_frame = {}
-product_code_frame={'Code': '',
-                     'Batch and expire': '',
-                     'Product Code': '',
-                     'Product name': '',
-                     'Box': '',
-                     'Pallet': '',
-                     'Batch': '',
-                     'Expire date': ''}
-row_to_update = 'Code'
+# product_code_frame = {}
+# product_code_frame={'Code': '',
+#                      'Batch and expire': '',
+#                      'Product Code': '',
+#                      'Product name': '',
+#                      'Box': '',
+#                      'Pallet': '',
+#                      'Batch': '',
+#                      'Expire date': ''}
+# row_to_update = 'Code'
 
-try: 
-   oldvalue = product_code_frame[row_to_update] 
-except: 
-   oldvalue = ''
+# try: 
+#     oldvalue = product_code_frame[row_to_update] 
+# except: 
+#     oldvalue = ''
 
-# to increment a value
+# # to increment a value
 
-#    try: 
-#       newval = int(oldval) 
-#       newval += 1
-#    except: 
-#       newval = 1 
+#     try: 
+#         newval = int(5)
+#         newval += 1
+#     except:
+#         newval = 1 
+
+#     product_code_frame[row_to_update] = "%s" % newval
+
+# # to append a value  
+
+#     try:
+#         newval = int(5) 
+#         newval += 1
+#     except:
+#         newval = 1 
 
 
-#    drug_dictionary[row_to_update] = "%s" % newval
+#     product_code_frame[row_to_update] = "%s,%s" % (5,newval)
 
-# to append a value  
-
-   try: 
-      newval = int(oldval) 
-      newval += 1
-   except: 
-      newval = 1 
-
-
-   product_code_frame[row_to_update] = "%s,%s" % (oldval,newval)
+# print("'%s':'%s'" % ( row_to_update, product_code_frame[row_to_update]))
 
 
 
