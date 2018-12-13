8566be83d8c2e45694068bbacaa5f75a47153b53
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-13 21:49:54 +0200

    working files removed

--- word_to_numbers.py
+++
@@ -1,7 +0,0 @@
-import string
-
-
-list1 = list('Panangin')
-list2 = list(string.ascii_letters[:])
-result=[(j) for i in range(len(list1)) for j in range(len(list2)) if list1[i] == list2[j]]
-print(result)
