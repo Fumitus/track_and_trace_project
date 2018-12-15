280fa9c5b878b9b186cf7ab3f1c820fcc76f845f
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-16 01:08:43 +0200

    TaT.cvs is working

--- TaT.py
+++ TaT.py
@@ -1,5 +1,7 @@
 import sys
 import argparse
+import csv
+
 
 def main(passed_args=None):
 
@@ -30,18 +32,24 @@ def counter(func):
         wrapper.count += 1
         res = func(*args, **kwargs)
         print("{0} has been used: {1}x".format(func.__name__, wrapper.count))
-        print(res)
         return res
     wrapper.count = 0
+    print(wrapper())
     return wrapper
 
 name, batch, expire = main()
 
 def join_product_code_data(name, batch, expire):
+    """
+    Function to join product data from input
+    """
     product = name + batch + expire
     return product
 
 def open_not_used_codes(filename='codes.txt'):
+    """
+    Function to read unique codes from a file
+    """
     file = open(filename, 'r')
     lines = file.read().split('\n')
     file.close()
@@ -52,28 +60,51 @@ lines = open_not_used_codes()
 print(lines)
 
 @counter
-def join_product_code(product, lines):
+def join_product_code(product=product, lines=lines):
+    """"
+    Function to produce `product_code` 
+    from uniques code and data from input
+    """
     first_line = lines[0]
     product_code = product + '/' + first_line
     return product_code
 
-product_code = join_product_code(product=product, lines=lines)
+product_code = join_product_code()
 print(product_code)
 
 @counter
-def create_box_code(product, lines):
+def create_box_code(product=product, lines=lines, box_size=3):
+    """"
+    Function to produce `box_code` 
+    from uniques code, data from input and box size.
+    """
     first_line = lines[0]
     box_code = product + '/' + first_line + '/box'
     return box_code
 
+
+box_code = create_box_code()
+
+
+
 @counter
-def create_pallet_code(product, lines):
+def create_pallet_code(product=product, lines=lines, box_size=3):
+    """"
+    Function to produce `pallet_code` 
+    from uniques code, data from input and pallet size.
+    """
     first_line = lines[0]
+
+    
     pallet_code = product + '/' + first_line + '/pallet'
     return pallet_code
 
+pallet_code = create_pallet_code()
+
 def create_used_codes_reg(new_filename='used_codes.txt'):
-    
+    """
+    Function to register used unique codes.
+    """
     file = open(new_filename, 'a')
     lines = open_not_used_codes()
     n_lines = lines[0]
@@ -84,6 +115,10 @@ def create_used_codes_reg(new_filename='used_codes.txt'):
 n_lines = create_used_codes_reg()
 
 def delete_used_codes(filename='codes.txt'):
+    """
+    Function to delete used unique codes 
+    from a not used codes list.
+    """
     file = open(filename, 'r')
     contents = file.readlines()
     file.close()
@@ -99,7 +134,10 @@ def delete_used_codes(filename='codes.txt'):
 filename = delete_used_codes()
 
 def create_product_codes_reg(product_code, new_filename='product_codes.txt'):
-    
+    """
+    Function to create .txt and record 
+    unique product codes to it.
+    """
     file = open(new_filename, 'a')
     product_code_lines = product_code
     
@@ -107,24 +145,30 @@ def create_product_codes_reg(product_code, new_filename='product_codes.txt'):
     file.close()
     return product_code_lines
 
-# PRODUCT_CODES_GENERATED = 0
-# BOX_CODES_GENERATED = 0
-# PALLET_CODES_GENERATED = 0
-
-# def code_counter():
-#     global PRODUCT_CODES_GENERATED
-#     PRODUCT_CODES_GENERATED +=1
-
-#     global BOX_CODES_GENERATED
-#     global PALLET_CODES_GENERATED
- 
-#     BOX_CODES_GENERATED +=1
-#     PALLET_CODES_GENERATED +=1
-
-#     pass
-
 product_code_lines = create_product_codes_reg(product_code)
 
+def TaT_data_csv(filename='Tat_data.csv'):
+    import os.path
 
+    """
+    Function to create .csv file and record 
+    1. used unique codes from file
+    2. generated unique product_codes
+    3. box_codes
+    4. pallete_codes
+    """
 
+    file_exist = os.path.isfile(filename)
+    
+    with open(filename, 'a', newline='') as csvfile:
+        
+        headers = ['codes','product_codes', 'box_codes', 'pallet_codes']
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        
+        if not file_exist:
+            writer.writeheader() # file doesn't exist yet, write a header
+        
+        writer.writerow({'codes': lines[0], 'product_codes': product_code_lines, 'box_codes': box_code, 'pallet_codes': pallet_code})
+
+TaT_data_csv()
 
