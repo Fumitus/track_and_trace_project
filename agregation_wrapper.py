b39649fb6e3ba4647ca856cf1e7721501781375e
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-16 17:43:46 +0200

    files added

--- agregation_wrapper.py
+++ agregation_wrapper.py
@@ -1,13 +1,12 @@
 def agregation(func_to_agregate):
     def box_dict():
-        box_list = ['APAP20181215202012/100,025/box']
+        box_list = ['APAP20181215202012/100,025/box', 'APAP20181215202012/100,025/box', 'APAP20181215202012/100,025/box']
         product_code_list = ['APAP20181212202012/100,027',
                             'APAP20181212202012/100,028',
                             'APAP20181212202012/100,029']
 
-        for box_dictionary in product_code_list:
-            box_dictionary = dict(zip(box_list, product_code_list))
-            print(box_dictionary)
+        box_dictionary = dict(zip(box_list, product_code_list))
+        print(box_dictionary)
 
         func_to_agregate()
 
