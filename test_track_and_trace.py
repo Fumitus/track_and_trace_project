fdcde978142151cdc5b12e0af6cadd3fa43f5450
Aidis Stukas <aidiss@gmail.com>
2018-12-22 00:21:02 +0200

    Remove all prints from tests

--- test_track_and_trace.py
+++ test_track_and_trace.py
@@ -6,45 +6,43 @@ class TestCase(unittest.TestCase):
     def test_join_product_code_data(self):
         result = track_and_trace.join_product_code_data('a', 'b', 'c')
         self.assertEqual(result, 'abc')
-        print(result)
-  
-        
+
     def test_read_codes(self):
         box_size = 6
         filename = "data/codes.txt"
-        expected_result = ['100102', '100103', '100104', '100105', '100106', '100107']
+        expected_result = ['100102', '100103',
+                           '100104', '100105', '100106', '100107']
         result = track_and_trace.read_codes(box_size, filename)
         self.assertEqual(result, expected_result)
 
     def test_join_product_code(self):
         result = track_and_trace.join_product_code('a', 1)
         self.assertEqual(result, 'a/1')
-        print(result)
 
     def test_create_box_code(self):
         result = track_and_trace.create_box_code('a', 'b')
         self.assertEqual(result, 'a/b/box')
-        print(result)
 
     def test_product_code_group(self):
 
         box_code = 'APAP20181215202012/100,025/box'
-        product_code_list = ['APAP20181212202012/100,027', 'APAP20181212202012/100,028', 'APAP20181212202012/100,029']
+        product_code_list = ['APAP20181212202012/100,027',
+                             'APAP20181212202012/100,028', 'APAP20181212202012/100,029']
 
-        expected_result = {'APAP20181215202012/100,025/box': ['APAP20181212202012/100,027', 'APAP20181212202012/100,028', 'APAP20181212202012/100,029']}
-        result = track_and_trace.product_code_group(box_code, product_code_list)
+        expected_result = {'APAP20181215202012/100,025/box': [
+            'APAP20181212202012/100,027', 'APAP20181212202012/100,028', 'APAP20181212202012/100,029']}
+        result = track_and_trace.product_code_group(
+            box_code, product_code_list)
         self.assertEqual(result, expected_result)
 
     def test_product_codes_to_list(self):
         product = 'a'
         codes = ['123', '456', '789', '101112', '131415', '161718']
-        
-        expected_result = ['a/456', 'a/789', 'a/101112', 'a/131415', 'a/161718'], 'a/123/box'
+
+        expected_result = ['a/456', 'a/789', 'a/101112',
+                           'a/131415', 'a/161718'], 'a/123/box'
         result = track_and_trace.product_codes_to_list(product, codes)
-        print(expected_result)
-        print(result)
         self.assertEqual(result, expected_result)
-            
 
 
 unittest.main()
