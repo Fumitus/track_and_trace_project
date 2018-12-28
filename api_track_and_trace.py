652cf2f05993903d9621fa5658836cc1bf2a85ad
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-28 15:40:57 +0200

    api_track_and_trace updated

--- api_track_and_trace.py
+++ api_track_and_trace.py
@@ -1,62 +1,58 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
-app = Flask(__name__)
-api = Api(app)
+api_track_and_trace = Flask(__name__)
+api = Api(api_track_and_trace)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'}
-}
+PRODUCT_CODES = {}
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_product_code_doesnt_exist(product_code_id):
+    if product_code_id not in PRODUCT_CODES:
+        abort(404, message="TodoProduct_code {} doesn't exist".format(product_code_id))
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
 
-# Todo
-# shows a single todo item and lets you delete a todo item
+# ProductCode
+# shows a PRODUCT_CODES item and lets you to use items for box or pallet formation
 
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+class ProductCode(Resource):
+    def get(self, product_code_id):
+        abort_if_product_code_doesnt_exist(product_code_id)
+        return PRODUCT_CODES[product_code_id]
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
+    def delete(self, product_code_id):
+        abort_if_product_code_doesnt_exist(product_code_id)
+        del PRODUCT_CODES[product_code_id]
         return '', 204
 
-    def put(self, todo_id):
+    def put(self, product_code_id):
         args = parser.parse_args()
         task = {'task': args['task']}
-        TODOS[todo_id] = task
+        PRODUCT_CODES[product_code_id] = task
         return task, 201
 
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 
-class TodoList(Resource):
+class ProductCodeList(Resource):
     def get(self):
-        return TODOS
+        return PRODUCT_CODES
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        product_code_id = int(max(PRODUCT_CODES.keys()).lstrip('todo')) + 1
+        product_code_id = 'todo%i' % product_code_id
+        PRODUCT_CODES[product_code_id] = {'task': args['task']}
+        return PRODUCT_CODES[product_code_id], 201
 
 ##
 ## Actually setup the Api resource routing here
 ##
 
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(ProductCodeList, '/product_code')
+api.add_resource(ProductCode, '/product_code/<product_code_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    api_track_and_trace.run(debug=True)
