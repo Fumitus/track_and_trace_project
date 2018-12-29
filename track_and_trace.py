804414ce88106d7e3fa683e2795c46966299f77b
Andrius Baikštis <andrius.baikstis@biotecha.lt>
2018-12-29 23:27:21 +0200

    sqlLite function under cunstroction

--- track_and_trace.py
+++ track_and_trace.py
@@ -172,29 +172,19 @@ def main(passed_args=None):
     create_product_codes_reg(box)
     delete_codes(box_size)
     database = "data/sqlite.db"
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
+    sql_create_box_table = """ CREATE TABLE IF NOT EXISTS box_table (
+                                        box,
+                                        codes,
+                                        product_codes_list,
+                                        box_code
                                     ); """
  
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                                );"""
+    
     conn = create_connection(database)
     if conn is not None:
         # create projects table
-        create_table(conn, sql_create_projects_table)
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
+        create_table(conn, sql_create_box_table)
+        
     else:
         print("Error! Cannot create the database connection.")
 
